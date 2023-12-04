44ff9ea4e0fc7168983a2524905a5bfe67acb55e
Kseniia Oliveri <ksenia081096@gmail.com>
2023-12-04 12:05:03 +0100

    first commit

---
+++ src/UniqueChars.php
@@ -0,0 +1,26 @@
+<?php
+namespace Src;
+
+class UniqueChars 
+{   
+    public static $cache = [];
+    public function CountUniqueChars(string $string): int
+    {   
+        if(!key_exists($string, self::$cache))
+        {
+            echo PHP_EOL . "work in progress on $string";
+             $strToArr = mb_str_split($string);
+        $count = array_count_values($strToArr);
+        $uniqChars = [];
+        foreach($count as $num)
+        {
+            if($num == 1)
+            {
+                $uniqChars[] = $num;
+            }
+        }
+         self::$cache[$string] = count($uniqChars);
+        }
+      return self::$cache[$string];
+    }   
+}
